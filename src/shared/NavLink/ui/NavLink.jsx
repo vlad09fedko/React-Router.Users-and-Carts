c23f998628c8efe84f5820c4335ef4d008b9f607
foramer <vlad09fedko@gmail.com>
2026-08-16 01:40:26 +0300

    Redo navbar

--- src/shared/NavLink/ui/NavLink.jsx
+++ src/shared/NavLink/ui/NavLink.jsx
@@ -1,17 +1,22 @@
 import { NavLink } from 'react-router';
 import { Typography } from '@mui/material';
 
+import styles from '../styles/navLink.module.css';
+
 export default function ({ text, ...props }) {
   return (
     <NavLink
+      className={styles.link}
       {...props}
       style={({ isActive }) => ({
-        border: '2px solid black',
-        borderRadius: '5em',
-        padding: '0.5em 1.5em',
+        border: '2px dashed #1976d2',
+        borderRadius: '1.5em',
+        padding: '0.5em 2.5em',
         maxWidth: 'fit-content',
         textDecoration: 'none',
-        color: isActive ? 'green' : 'red',
+        transition: '0.3s',
+        color: 'black',
+        backgroundColor: isActive ? '#f0f0f0' : '#ffffff',
       })}>
       <Typography variant='h6'>{text}</Typography>
     </NavLink>
