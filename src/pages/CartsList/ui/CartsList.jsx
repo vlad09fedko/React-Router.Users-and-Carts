363339514560e6e11d60a6ac9d0f9e605bbeb0f8
foramer <vlad09fedko@gmail.com>
2026-08-16 00:58:41 +0300

    Add carts list

--- src/pages/CartsList/ui/CartsList.jsx
+++ src/pages/CartsList/ui/CartsList.jsx
@@ -1,5 +1,52 @@
+import { NavLink } from 'react-router';
+import { List, ListItem, Typography } from '@mui/material';
+
+import { useGetAllCartsQuery } from '@/entities/carts/api/cartApi.js';
+
+import styles from '../styles/cartsList.module.css';
+
 function Carts() {
-  return <>Carts</>;
+  const { data, isLoading } = useGetAllCartsQuery();
+  const carts = data?.carts ?? [];
+
+  if (isLoading)
+    return (
+      <Typography variant='h3' align='center' sx={{ padding: '1em' }}>
+        Loading...
+      </Typography>
+    );
+
+  return (
+    <List
+      sx={{
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '1em',
+      }}>
+      {carts.map(({ id, total }) => (
+        <NavLink to={`/carts/${id}`} key={id} className={styles.link}>
+          <ListItem
+            sx={{
+              border: '2px dashed #1976d2',
+              borderRadius: '1em',
+              padding: '1em 10em',
+              transition: '0.3s',
+              cursor: 'pointer',
+              color: 'black',
+              textDecoration: 'none',
+              '&:hover': {
+                backgroundColor: '#f0f0f0',
+              },
+            }}>
+            <Typography>
+              Cart #{id} Total - {total} $
+            </Typography>
+          </ListItem>
+        </NavLink>
+      ))}
+    </List>
+  );
 }
 
 export default Carts;
