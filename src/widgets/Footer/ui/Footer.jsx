9c3a13929b484d801ae8062dca3eb5c29efeb039
foramer <vlad09fedko@gmail.com>
2026-08-15 15:26:15 +0300

    Add footer

--- src/widgets/Footer/ui/Footer.jsx
+++ src/widgets/Footer/ui/Footer.jsx
@@ -1,10 +1,23 @@
-import { Box, Typography } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 
 function Footer() {
   return (
-    <Box>
-      <Typography>all rights reserved</Typography>
-    </Box>
+    <Stack
+      sx={{
+        flexGrow: 1,
+        textAlign: 'center',
+        boxShadow:
+          '0px 2px 4px -1px rgba(0,0,0,0.2),0px 4px 5px 0px rgba(0,0,0,0.14),0px 1px 10px 0px rgba(0,0,0,0.12)',
+        backgroundColor: '#1976d2',
+        color: '#ffffff',
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        minHeight: '2em',
+      }}>
+      <Typography variant='h6'>all rights reserved</Typography>
+    </Stack>
   );
 }
 
